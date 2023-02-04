266522dcaa3b47c70d2948fe55ba3ff77358d4fd
penggeng9100 <57079054+penggeng9100@users.noreply.github.com>
2023-02-03 22:08:27 -0600

    Update CubeGen.cs

--- CubeGen.cs
+++ CubeGen.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class CubeGen : MonoBehaviour
 {
-    public GameObject cubePrefab;
+    public GameObject cubePrefab; //new game object
+    public float scalingFactor = 0.95f;// amount each cube will shrink each frame
+    public int numberOfCubes = 0;//Total number of cubes instatied
+    public List<GameObject> gameObjectList; // list for all the cubes 
+
     // Start is called before the first frame update
     void Start()
     {
+        gameObjectList = new List<GameObject>();// instated the list 
+
         // Instantiate(cubePrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(cubePrefab);
+        numberOfCubes++; // add to the number of cubes
+        GameObject gObj = Instantiate<GameObject>(cubePrefab); // craetes cube instance 
+        gObj.name = "Cube" + numberOfCubes;// name of cube instance 
+        Color randColor = new Color(Random.value, Random.value, Random.value); // creates a new random colar 
+        gObj.GetComponent<Renderer>().material.color = randColor; // assigns random color to game object 
+        gObj.transform.position = Random.insideUnitSphere;//random location inside a sphere radius of 1 out from 0,0,0 
+        gameObjectList.Add(gObj); // add to list 
+        List<GameObject> removeList = new List<GameObject> ();// list for removed objects 
+        foreach(GameObject goTemp in gameObjectList ){ 
+        float scale = goTemp.transform.localScale.x;//records current scale  
+        scale *= scalingFactor; // scale multipled by scale factor 
+        goTemp.transform.localScale = Vector3.one * scale; //transform scale 
+        if(scale <= 0.1f) 
+      { 
+         removeList.Add(goTemp); 
+        }// end if (scale < 0.1f) 
+      }// end foreach (GameObject goTemp in gameObjectList) 
+        foreach (GameObject goTemp in removeList) 
+ { 
+        gameObjectList.Remove(goTemp); // remove from object list 
+        Destroy(goTemp);//destorys game object 
+        Debug.Log(removeList); 
+        }// end foreach (GameObject goTemp in removeList) 
+        //Instantiate(cubePrefab);
     }
 }
